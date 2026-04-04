2f8ac944380941a045c40579d5d56724a2333739
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2026-04-04 22:03:46 +0530

    Add: non-cyclical-number - submission-9

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-9.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isHappy(int n) {
+        //Linked List cycle detection
+        int slow = n;
+        int fast = n;
+
+        //always end up in cycle either of 1 or else
+        while(true){
+            slow = sqrOfDigits(slow);
+            fast = sqrOfDigits(sqrOfDigits(fast));
+            if(slow == fast){
+                break;
+            }
+        }
+
+        return slow == 1;
+        
+    }
+
+    private int sqrOfDigits(int n){
+        int res = 0;
+        while(n != 0){
+            int digit = n%10;
+            res += (digit * digit);
+            n = n/10;
+        }
+        return res;
+    }
+}
