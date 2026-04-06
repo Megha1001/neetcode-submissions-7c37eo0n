b026b49ad130518a9ebe7fcaab05c9c4a309e9fe
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2026-04-06 23:05:14 +0530

    Add: non-overlapping-intervals - submission-2

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-2.java
@@ -0,0 +1,26 @@
+//TC : O(NLogN), SC : O(1)
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        //Greedy Approach : The interval that ends earliest gives you the maximum room for future intervals
+        //sort by end
+        int n = intervals.length;
+
+        if(n==0){
+            return 0;
+        }
+
+        Arrays.sort(intervals, (a,b)-> (a[1] - b[1])); //O(NlogN)
+        int prevEnd = intervals[0][1];
+        int count = 0;
+        for(int i = 1; i < n; i++){ //O(N)
+            if(intervals[i][0] < prevEnd){ // not = since[1,2] , [2,3] are non-overlapping
+                ++count;
+            }else{
+                prevEnd = intervals[i][1];
+            }
+        }
+
+        return count;
+
+    }
+}
