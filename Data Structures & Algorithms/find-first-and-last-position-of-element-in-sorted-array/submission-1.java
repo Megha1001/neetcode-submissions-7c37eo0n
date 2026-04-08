a35282dd9a6a33e1786c75d2d7703be229e08b51
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2026-04-08 23:19:47 +0530

    Add: find-first-and-last-position-of-element-in-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-first-and-last-position-of-element-in-sorted-array/submission-1.java
@@ -0,0 +1,55 @@
+//TC : O(logN), SC : O(1)
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        /*
+        Find first occurence and find last occurence
+        */
+
+        int n = nums.length;
+        if(n == 0){
+            return new int[]{-1,-1};
+        }
+
+        //first occurrence
+        int l = 0;
+        int h = n-1;
+        int first = -1;
+
+        while(l <= h){
+            int m = l + (h - l)/2;
+            if(target == nums[m]){
+                first = m;
+                h = m - 1;
+            }else if(target > nums[m]){
+                l = m + 1;
+            }else{
+                h = m - 1;
+            }
+        }
+
+        if(first == -1){
+            return new int[]{-1,-1};
+        }
+
+        //second occurrence
+        int last = -1;
+        l = 0;
+        h = n-1;
+
+        while(l <= h){
+            int m = l + (h - l)/2;
+            
+            if(nums[m] == target){
+                last = m;
+                l = m + 1;
+            }else if(target > nums[m]){
+                l = m + 1;
+            }else {
+                h = m - 1;
+            }
+        }
+
+        return new int[]{first, last};
+        
+    }
+}
