bf7aee07eff1ae166962c512de6f9d21d7df6e44
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2026-04-10 13:45:25 +0530

    Add: capacity-to-ship-packages-within-d-days - submission-0

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int l = 0;
+        int r = 0;
+
+        for(int w : weights){
+            l = Math.max(l, w);
+            r += w;
+        }
+
+        int res = r;
+        while(l <= r){
+            int m = l + (r - l)/2;
+            if(canShip(weights, days, m)){
+                res = Math.min(res, m);
+                r = m-1;
+            }else{
+                l = m+1;
+            }
+        }
+
+        return res; 
+    }
+
+    private boolean canShip(int []weights, int days, int cap){
+        int ship = 1;
+        int currCap = cap;
+        for(int w : weights){
+            if(currCap - w < 0){
+                ++ship;
+                if(ship > days){
+                    return false;
+                }
+                currCap = cap;
+            }
+            currCap -= w;
+        }
+        return true;
+    }
+}
