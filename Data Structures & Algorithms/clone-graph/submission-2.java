e4e6e283ab2f6309d278f12f7ecd12e83b2038c5
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2026-04-11 17:31:24 +0530

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.java
@@ -0,0 +1,50 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        // BFS
+        if(node == null){
+            return null;
+        }
+
+        Deque<Node> q = new ArrayDeque<>();
+        Map<Node, Node> oldToNew = new HashMap<>();
+
+        q.offer(node);
+        oldToNew.put(node, new Node(node.val));
+
+        while(!q.isEmpty()){
+            Node curr = q.poll();
+
+            for(Node neighbor : curr.neighbors){
+                if(!oldToNew.containsKey(neighbor)){
+                    oldToNew.put(neighbor, new Node(neighbor.val));
+                    q.offer(neighbor);
+                }
+
+                oldToNew.get(curr).neighbors.add(oldToNew.get(neighbor));
+            }
+        }
+
+        return oldToNew.get(node);
+        
+    }
+}
