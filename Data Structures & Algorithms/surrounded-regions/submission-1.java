82c8ff70a6881078d75f4c2226ce85132caab696
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2026-04-12 22:20:18 +0530

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.java
@@ -0,0 +1,52 @@
+class Solution {
+    private int ROWS;
+    private int COLS;
+    private int[][]directions = {{0,1},{0,-1},{1,0},{-1,0}};
+    public void solve(char[][] board) {
+
+        ROWS = board.length;
+        COLS = board[0].length;
+
+        for(int c = 0; c < COLS; c++){
+            if(board[0][c] == 'O'){
+                dfs(board, 0, c);
+            }
+            if(board[ROWS - 1][c] == 'O'){
+                dfs(board, ROWS - 1, c);
+            }
+        }
+        
+
+        for(int r = 0; r < ROWS; r++){
+            if(board[r][0] == 'O'){
+                dfs(board, r, 0);
+            }
+            if(board[r][COLS - 1] =='O'){
+                dfs(board, r, COLS - 1);
+            }
+        }
+
+
+        for(int r = 0; r < ROWS; r++){
+            for(int c = 0; c < COLS; c++){
+                if(board[r][c] == 'O'){
+                    board[r][c] = 'X';
+                }
+
+                else if(board[r][c] == 'T'){
+                    board[r][c] = 'O';
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][]board, int r, int c){
+        if(r < 0 || c < 0 || r >= ROWS || c >= COLS || board[r][c] != 'O'){
+            return;
+        }
+        board[r][c] = 'T';//mark as safe
+        for(int []dir : directions){
+            dfs(board, r + dir[0], c + dir[1]);
+        }
+    }
+}
