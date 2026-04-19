54f5b05f39f0ced05ddaeef8544d95d1b4fbc88f
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2026-04-19 18:35:50 +0530

    Add: delete-node-in-a-bst - submission-2

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-2.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ //TC : O(H)
+ // SC : O(J)
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        /*
+         - Node with value k is leaf delete it make value null;
+         - node has only one child -> replace it with the children value
+         - node has two child -> find successor or predecessor and delete the succ or predecessor node
+        */
+
+        if(root == null){
+            return null;
+        }
+
+        if(root.val > key){
+            //go to left
+            root.left = deleteNode(root.left, key);
+        }
+        else if(root.val < key){
+            //go to right
+            root.right = deleteNode(root.right, key);
+        }
+
+        else{
+            // found key
+
+            //have zero child or have only right child
+            if(root.left == null){
+                return root.right;
+            }else if(root.right == null){
+                return root.left;
+            }else{
+                //has both child
+                TreeNode succ = findSuccessor(root, key);
+                root.val = succ.val;
+                root.right = deleteNode(root.right, succ.val);
+            }
+        }
+
+        return root;
+        
+    }
+
+    private TreeNode findSuccessor(TreeNode root, int key){
+        TreeNode curr = root.right;
+        while(curr != null && curr.left != null){
+            curr = curr.left;
+        }
+        return curr;
+    }
+}
