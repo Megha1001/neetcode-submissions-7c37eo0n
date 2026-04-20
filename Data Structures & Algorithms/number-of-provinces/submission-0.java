a6be9054f98ef36389c7d1dbf5339897263f145d
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2026-04-20 09:53:19 +0530

    Add: number-of-provinces - submission-0

---
+++ Data Structures & Algorithms/number-of-provinces/submission-0.java
@@ -0,0 +1,27 @@
+//TC : O(N)
+// SC : O(N)
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int provinces = 0;
+
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                dfs(isConnected, i, visited);
+                ++provinces;
+            }
+        }
+        return provinces;
+    }
+
+    private void dfs(int[][]isConnected, int i, boolean[]visited){
+        visited[i] = true;
+
+        for(int j = 0; j < isConnected.length; j++){
+            if(isConnected[i][j] == 1 && !visited[j]){
+                dfs(isConnected, j, visited);
+            }
+        }
+    }
+}
