4d9a61bae1b98444d50d409915350512a7551cbf
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2026-04-22 21:23:37 +0530

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.java
@@ -0,0 +1,68 @@
+//TC : O((V+E) * E); // *E for each edge we run hasPath = E~V = O(V*V) = O(N*N)
+//SC : O(V+E)
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for(int i = 0; i <= n; i++){ // =n since 1...n  // TC : O(V), SC : (V)
+            adj.add(new ArrayList<>());
+        }
+
+        for(int e[] : edges){ // TC : O(E) for each edge we run hasPath
+            int u = e[0];
+            int v = e[1];
+
+            boolean[]visited = new boolean[n+1]; // n+1 since 1...n , SC : (V)
+
+            if(!adj.get(u).isEmpty() && !adj.get(v).isEmpty() && hasPath(adj, u, v, visited)){
+                return e;
+            }
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        return new int[0];
+        
+    }
+
+    //DFS
+    // private boolean hasPath(List<List<Integer>>adj, int u, int v, boolean[]visited){
+    //     if(u == v){
+    //         return true;
+    //     }
+    //     visited[u] = true;
+    //     for(int nei : adj.get(u)){ // SC : O(E)
+    //         if(!visited[nei]){
+    //             if(hasPath(adj, nei, v, visited)){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+
+    //     return false;
+    // }
+
+    //BFS
+    private boolean hasPath(List<List<Integer>>adj, int u, int v, boolean[]visited){
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(u);
+
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            visited[curr] = true;
+            if(curr == v){
+                return true;
+            }
+            for(int nei : adj.get(curr)){
+                if(!visited[nei]){
+                    q.offer(nei);
+                }
+            }
+        }
+
+        return false;
+        
+    }
+}
