239c24b4c99d46119d44d8e64bb2119578648713
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2026-04-25 16:25:28 +0530

    Add: daily-temperatures - submission-5

---
+++ Data Structures & Algorithms/daily-temperatures/submission-5.java
@@ -0,0 +1,20 @@
+// TC : O(N)
+// SC : O(N)
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        //Monotonic stack from R to L -> decreasing
+        int n = temperatures.length;
+        int res [] = new int[n];
+        Deque<Integer> st = new ArrayDeque<>();
+
+        for(int i = n-1; i >= 0; i--){
+            while(!st.isEmpty() && temperatures[st.peek()] <= temperatures[i]){
+                st.pop();
+            }
+            res[i] = !st.isEmpty() ? st.peek() - i : 0;
+            st.push(i);
+        }
+        return res;
+        
+    }
+}
