287a0d1ade45558c843f563a7926f230d11e8f7e
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2026-05-16 19:05:27 +0530

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.java
@@ -0,0 +1,40 @@
+//TC : O(N*N*N), extra n for substring
+//SC : O(N)
+class Solution {
+    private int n;
+    private Set<String> set;
+    private int[] memo;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        n = s.length();
+        set = new HashSet<>(wordDict);
+        memo = new int[n + 1];
+        Arrays.fill(memo, -1);
+        return solve(s, 0);
+    }
+
+    private boolean solve(String s, int idx){
+        if(idx == n){
+            return true;
+        }
+        if(set.contains(s.substring(idx, n))){
+            return true;
+        }
+
+        if(memo[idx] != -1){
+            return memo[idx] == 1;
+        }
+
+        for(int len = 1; len <= n; len++){
+            if(idx + len > n){
+                break;
+            }
+            String temp = s.substring(idx, idx + len);
+            if(set.contains(temp) && solve(s, idx + len)){
+                memo[idx] = 1;
+                return true;
+            }
+        }
+        memo[idx] = 0;
+        return false;
+    }
+}
