180c2eeb18654058c4513f83e4db9e22d4839bf5
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2026-05-17 12:57:54 +0530

    Add: decode-ways - submission-3

---
+++ Data Structures & Algorithms/decode-ways/submission-3.java
@@ -0,0 +1,34 @@
+class Solution {
+    private int n;
+    private int[]memo;
+    public int numDecodings(String s) {
+        //Top down approach
+        n = s.length();
+        memo = new int[101];
+        Arrays.fill(memo, -1);
+
+        return solve(0, s);
+    }
+
+    private int solve(int idx, String s){
+        if(memo[idx] != -1){
+            return memo[idx];
+        }
+
+        if(idx == n){
+            return memo[idx] = 1;
+        }
+
+        if(s.charAt(idx) == '0'){
+            return memo[idx] = 0;
+        }
+
+        int result = solve(idx + 1, s);
+        if(idx + 1 < n){
+            if(s.charAt(idx) == '1' || (s.charAt(idx) == '2' && s.charAt(idx + 1) <= '6')){
+                result += solve(idx + 2, s);
+            }
+        }
+        return memo[idx] = result;
+    }
+}
