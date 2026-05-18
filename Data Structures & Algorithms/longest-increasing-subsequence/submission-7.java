b0c6cdc4dae5ea8f0e348a2592f32dd1a1852ffd
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2026-05-18 13:17:44 +0530

    Add: longest-increasing-subsequence - submission-7

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-7.java
@@ -0,0 +1,40 @@
+//TC : O(N*N)
+//SC : O(N*N)
+class Solution {
+    private int n;
+    private int[][]memo;
+    public int lengthOfLIS(int[] nums) {
+        n = nums.length;
+        memo = new int[1001][1001];
+
+        for(int[]row : memo){
+            Arrays.fill(row, -1);
+        }
+        
+        return solve(0, -1, nums);
+    }
+
+    private int solve(int currIdx, int prevIdx, int[]nums){
+        if(currIdx == n){
+            return 0;
+        }
+
+        if(prevIdx != -1 && memo[currIdx][prevIdx] != -1){
+            return memo[currIdx][prevIdx];
+        }
+
+        int take = 0;
+        if(prevIdx == -1 || nums[currIdx] > nums[prevIdx]){
+            take = 1 + solve(currIdx + 1, currIdx, nums);
+        }
+
+        int skip = solve(currIdx + 1, prevIdx, nums);
+
+        if(prevIdx != -1){
+            memo[currIdx][prevIdx] = Math.max(take, skip);
+        }
+
+        return Math.max(take, skip);
+
+    }
+}
