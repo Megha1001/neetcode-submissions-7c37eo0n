00921d5b541e934c02dbd7cf734ee606bb92cc9d
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2026-05-24 14:11:08 +0530

    Add: triangle - submission-1

---
+++ Data Structures & Algorithms/triangle/submission-1.java
@@ -0,0 +1,23 @@
+//TC : O(N*N)
+//SC : O(N*N)
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        //Bottom up
+        int n = triangle.size();
+
+        int dp [][] = new int[n][n];
+
+        for(int i = 0; i < n; i++){
+            dp[n - 1][i] = triangle.get(n-1).get(i);
+        }
+
+        for(int row = n-2; row >= 0; row--){
+            for(int col = 0; col <= row; col++){
+                dp[row][col] = triangle.get(row).get(col) + Math.min(dp[row + 1][col], dp[row + 1][col + 1]);
+            }
+        }
+
+        return dp[0][0];
+        
+    }
+}
