92af575371b130895f72660d557d4e19c5d8a6c6
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2026-05-24 17:14:49 +0530

    Add: burst-balloons - submission-0

---
+++ Data Structures & Algorithms/burst-balloons/submission-0.java
@@ -0,0 +1,37 @@
+class Solution {
+    private int[][]memo;
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[]arr = new int[n + 2];
+        arr[0] = arr[n + 1] = 1;
+
+        for(int i = 0; i < n; i++){
+            arr[i + 1] = nums[i];
+        }
+
+        memo = new int[n + 2][n + 2];
+
+        return solve(arr, 0, n + 1);
+    }
+
+    private int solve(int[]arr, int left, int right){
+        if(right - left <= 1){
+            return 0;
+        }
+
+        if(memo[left][right] != 0){
+            return memo[left][right];
+        }
+
+        int best = 0;
+
+        for(int k = left + 1; k < right ; k++){
+            int coins = solve(arr, left, k) +
+                        arr[left] * arr[k] * arr[right] + 
+                        solve(arr, k , right);
+            best = Math.max(best, coins);
+        }
+
+        return memo[left][right] = best;
+    }
+}
