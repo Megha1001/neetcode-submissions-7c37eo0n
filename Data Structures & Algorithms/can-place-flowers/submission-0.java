7ea4439249065dd07c1a4378dcb78b1a1ac5dfd4
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2026-07-21 20:55:19 +0530

    Add: can-place-flowers - submission-0

---
+++ Data Structures & Algorithms/can-place-flowers/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        //Greedy approach
+        int count = 0;
+
+        for(int i = 0; i < flowerbed.length; i++){
+            int left = (i==0) ? 0 : flowerbed[i - 1];
+            int right = (i == flowerbed.length - 1) ? 0 : flowerbed[i + 1];
+
+            if(flowerbed[i] == 0 && left == 0 && right == 0){
+                flowerbed[i] = 1;
+                ++count;
+
+                if(count >= n){
+                    return true;
+                }
+            }
+            
+        }
+        return count >= n;
+    }
+}
